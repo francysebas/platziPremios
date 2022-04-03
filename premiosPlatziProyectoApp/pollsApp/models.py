fa4290e5e2d8eb8f2b7da7c30a0cbd06dc18f4b5
francysebas <francyn@unicauca.edu.co>
2022-04-03 12:07:48 -0500

    creacion de modelos

--- premiosPlatziProyectoApp/pollsApp/models.py
+++ premiosPlatziProyectoApp/pollsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Pregunta(models.Model):
+    pregunta_text = models.CharField('pregunta', max_length=100)
+    publicacion_fecha= models.DateTimeField("fecha publicacion")
+
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    respuest_text = models.CharField(max_length=100)
+    votos = models.IntegerField(default=0)
