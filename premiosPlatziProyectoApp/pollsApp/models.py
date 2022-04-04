6189e2e36d0a421bd68f093b8c7d791b404f44d6
francysebas <francyn@unicauca.edu.co>
2022-04-03 21:49:59 -0500

    models, admin

--- premiosPlatziProyectoApp/pollsApp/models.py
+++ premiosPlatziProyectoApp/pollsApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 # Create your models here.
@@ -7,8 +9,16 @@ class Pregunta(models.Model):
     pregunta_text = models.CharField('pregunta', max_length=100)
     publicacion_fecha= models.DateTimeField("fecha publicacion")
 
+    def __str__(self):
+        return self.pregunta_text
+
+    def was_published_recently(self):
+        return self.publicacion_fecha >= timezone.now() - datetime.timedelta(days=1)
 
 class Respuesta(models.Model):
     pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
     respuest_text = models.CharField(max_length=100)
     votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.respuest_text
