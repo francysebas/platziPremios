576d7b39b6037fbeb09150be0900d470cdf59115
francysebas <31090790+francysebas@users.noreply.github.com>
2022-04-05 16:22:37 -0500

    template, views

--- premiosPlatziProyectoApp/pollsApp/urls.py
+++ premiosPlatziProyectoApp/pollsApp/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-
+    # ex: /polls/
     path('', views.index, name="index"),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detalle, name="detalle"),
+    # ex: /polls/5/resultado
+    path('<int:question_id>/resultado/', views.resultado, name="resultado"),
+    # ex: /polls/5/voto
+    path('<int:question_id>/voto/', views.voto, name="voto"),
+
 
 ]
--- premiosPlatziProyectoApp/pollsApp/views.py
+++ premiosPlatziProyectoApp/pollsApp/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Pregunta
 
 def index(request):
-    return HttpResponse("hola mundo")
+    lates_respuesta_list = Pregunta.objects.all()
+    return render(request, "pollsApp/index.html", {
+        "lates_respuesta_list": lates_respuesta_list
+    })
+
+def detalle(request, question_id):
+    # pregunta = Pregunta.objects.get(pk=question_id)
+    pregunta = get_object_or_404(Pregunta, pk=question_id)
+    return render(request, "pollsApp/detalle.html",{
+        "pregunta":pregunta
+    })
+
+def resultado(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la  número {question_id}")
+
+def voto(request, question_id):
+    return HttpResponse(f"Estás votando oa la pregunta número {question_id}")
