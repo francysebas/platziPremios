c0a2aa560ec20b834ad7a67ebcfb1f01ad46d6f6
francysebas <31090790+francysebas@users.noreply.github.com>
2022-04-05 19:56:58 -0500

    resultado html, resulta view

--- premiosPlatziProyectoApp/pollsApp/views.py
+++ premiosPlatziProyectoApp/pollsApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Pregunta
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Pregunta, Respuesta
+from django.urls import reverse
 
 def index(request):
     lates_respuesta_list = Pregunta.objects.all()
@@ -16,7 +17,25 @@ def detalle(request, question_id):
     })
 
 def resultado(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la  número {question_id}")
+   # return HttpResponse(f"Estas viendo los resultados de la  número {question_id}")
+   pregunta = get_object_or_404(Pregunta, pk=question_id)
+   return render(request, "pollsApp/resultado.html/",{
+       "pregunta":pregunta
+   })
 
 def voto(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+   # return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    pregunta = get_object_or_404(Pregunta, pk=question_id)
+    try:
+        seleccionar_respuesta= pregunta.respuesta_set.get(pk=request.POST["respuesta_form"])
+    except(KeyError, Respuesta.DoesNotExist):
+        return render(request, "pollsApp/detalle.html", {
+            "pregunta": pregunta,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        
+        seleccionar_respuesta.votos +=1
+        seleccionar_respuesta.save()
+        return HttpResponseRedirect(reverse("polls:resultado", args=(question_id,)))
+
